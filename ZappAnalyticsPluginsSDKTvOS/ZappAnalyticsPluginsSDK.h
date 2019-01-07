85eb457118f56f23704a243946dc9a8386233e46
kononenkoAnton <a.kononenko@applicaster.com>
2019-01-06 21:08:40 -0500

    Update

---
+++ ZappAnalyticsPluginsSDKTvOS/ZappAnalyticsPluginsSDK.h
@@ -0,0 +1,20 @@
+//
+//  ZappAnalyticsPluginsSDKTvOS.h
+//  ZappAnalyticsPluginsSDKTvOS
+//
+//  Created by Anton Kononenko on 1/6/19.
+//  Copyright © 2019 Applicaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZappAnalyticsPluginsSDKTvOS.
+FOUNDATION_EXPORT double ZappAnalyticsPluginsSDKVersionNumber;
+
+//! Project version string for ZappAnalyticsPluginsSDKTvOS.
+FOUNDATION_EXPORT const unsigned char ZappAnalyticsPluginsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZappAnalyticsPluginsSDKTvOS/PublicHeader.h>
+
+#import <ZappAnalyticsPluginsSDK/APAnalyticsProvider.h>
+#import <ZappAnalyticsPluginsSDK/ZPPlayerControllsNotifications.h>
